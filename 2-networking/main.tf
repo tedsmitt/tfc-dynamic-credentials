1066be9df46b1459e863d8fdab082b2ec60cbc63
Ed Smith <edwardsmith92@outlook.com>
2023-03-16 18:30:46 +0000

    feat: update vpc module call to build subnets

--- 2-networking/main.tf
+++ 2-networking/main.tf
@@ -2,10 +2,18 @@ variable "stage" {
   type = string
 }
 
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "vpc-${var.stage}"
-  cidr   = "10.0.0.0/16"
+  source         = "terraform-aws-modules/vpc/aws"
+  name           = "vpc-${var.stage}"
+  cidr           = "10.0.0.0/16"
+  azs            = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Name = "vpc-${var.stage}"
+  }
 }
 
 output "vpc" {
